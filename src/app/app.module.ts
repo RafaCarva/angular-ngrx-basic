5c9d64b7c2063f4f62e2a2a7f1bb2be31847b587
Rafael Faria Carvalho <rafael.carvalho@programmers.com.br>
2019-10-29 14:02:04 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MaterialModule } from './material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonComponent } from './person/person.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
